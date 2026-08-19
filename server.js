41c13dd79cae5ef0e056c92a5d60f34d88242d01
Gio <gioini197@gmail.com>
2026-08-19 14:03:13 +0800

    Gunakan split env variables untuk firebase

--- server.js
+++ server.js
@@ -8,18 +8,12 @@ const midtransClient = require('midtrans-client');
 const cookieParser = require('cookie-parser');
 const jwt = require('jsonwebtoken');
 const admin = require("firebase-admin");
-let serviceAccount;
-try {
-    if (!process.env.FIREBASE_SERVICE_ACCOUNT) {
-        throw new Error("Variabel FIREBASE_SERVICE_ACCOUNT kosong atau belum diset di Railway!");
-    }
-    serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT.trim());
-} catch (e) {
-    console.error("ERROR FIREBASE:", e.message);
-    process.exit(1); // Menghentikan server dengan aman agar error-nya jelas terbaca di log
-}
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n') : undefined,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL
+  }),
   databaseURL: "https://vending-machine-a267f-default-rtdb.asia-southeast1.firebasedatabase.app"
 });
 
